56eb9e872ec3e1b8426a27878792d9b2aa12c130
Sundeep Mediratta <smedius@gmail.com>
2022-07-15 18:43:01 -0400

    Include Ubuntu install in local install script

--- local_install.sh
+++ local_install.sh
@@ -6,3 +6,11 @@ mkdir .build
 meson --prefix=$HOME/.local/ --localedir=share/gnome-shell/extensions/ding@rastersoft.com/locale .build
 ninja -C .build install
 rm -rf .build
+
+if  ( cat /etc/lsb-release | grep -o 'Ubuntu 22.04 LTS' ); then
+    echo "Installing for Ubuntu 22.04...."
+    rm -rf ~/.local/share/gnome-shell/extensions/dingubuntu@rastersoft.com
+    mv ~/.local/share/gnome-shell/extensions/ding@rastersoft.com ~/.local/share/gnome-shell/extensions/dingubuntu@rastersoft.com
+    sed -i "s#ding@rastersoft#dingubuntu@rastersoft#" ~/.local/share/gnome-shell/extensions/dingubuntu@rastersoft.com/metadata.json
+    echo "Completed"
+fi
