15bcdcace0f884c44600c0d8e2b849458f5f3544
Sundeep Mediratta <smedius@gmail.com>
2022-08-11 19:50:53 -0400

    ding.js errors and formatting fixed with eslint using GJS/Gnome guidelines.

--- ding.js
+++ ding.js
@@ -32,36 +32,45 @@ let asDesktop = false;
 let primaryIndex = 0;
 let desktopVariants = [];
 let remoteDingActions;
+let data;
+let dataVariant;
 
-function print_usage() {
-    print("Desktop Icons NG");
-    print("Usage:");
-    print("  -h                      : show this help");
-    print("  -E                      : run as desktop (with transparent window, reading data from the extension...)");
-    print("  -P code path            : set the path where the code is stored");
-    print("  -M index                : index of the primary monitor");
-    print("  -V gnome version        : pass the gnome version to the DING application");
-    print("  -D x:y:w:h:z:t:b:l:r:i  : monitor data");
-    print("      x: X coordinate");
-    print("      y: Y coordinate");
-    print("      w: width in pixels");
-    print("      h: height in pixels");
-    print("      z: zoom value (must be greater than or equal to one)");
-    print("      t: top margin in pixels");
-    print("      b: bottom margin in pixels");
-    print("      l: left margin in pixels");
-    print("      r: right margin in pixels");
-    print("      i: monitor index (0, 1...)");
+/**
+ *
+ */
+function printUsage() {
+    print('Desktop Icons NG');
+    print('Usage:');
+    print('  -h                      : show this help');
+    print('  -E                      : run as desktop (with transparent window, reading data from the extension...)');
+    print('  -P code path            : set the path where the code is stored');
+    print('  -M index                : index of the primary monitor');
+    print('  -V gnome version        : pass the gnome version to the DING application');
+    print('  -D x:y:w:h:z:t:b:l:r:i  : monitor data');
+    print('      x: X coordinate');
+    print('      y: Y coordinate');
+    print('      w: width in pixels');
+    print('      h: height in pixels');
+    print('      z: zoom value (must be greater than or equal to one)');
+    print('      t: top margin in pixels');
+    print('      b: bottom margin in pixels');
+    print('      l: left margin in pixels');
+    print('      r: right margin in pixels');
+    print('      i: monitor index (0, 1...)');
 }
 
+/**
+ *
+ * @param {string} argv command line arguments
+ */
 function parseCommandLine(argv) {
     desktops = [];
-    for(let arg of argv) {
-        if (lastCommand == null) {
-            switch(arg) {
+    for (let arg of argv) {
+        if (lastCommand === null) {
+            switch (arg) {
             case '-h':
             case '-H':
-                print_usage();
+                printUsage();
                 errorFound = true;
                 break;
             case '-E':
@@ -81,52 +90,52 @@ function parseCommandLine(argv) {
             }
             continue;
         }
-        if (errorFound) {
+        if (errorFound)
             break;
-        }
-        switch(lastCommand) {
+
+        switch (lastCommand) {
         case '-P':
             codePath = arg;
             break;
         case '-D':
-            let data = arg.split(":");
-            if (data.length != 10) {
-                print("Incorrect number of parameters for -D\n");
-                print_usage();
+            data = arg.split(':');
+            if (data.length !== 10) {
+                print('Incorrect number of parameters for -D\n');
+                printUsage();
                 errorFound = true;
                 break;
             }
             if (parseFloat(data[4]) < 1.0) {
                 print("Error: ZOOM value can't be less than one\n");
-                print_usage();
+                printUsage();
                 errorFound = true;
                 break;
             }
             desktops.push({
-                x:parseInt(data[0]),
-                y:parseInt(data[1]),
-                width:parseInt(data[2]),
-                height:parseInt(data[3]),
-                zoom:parseFloat(data[4]),
-                marginTop:parseInt(data[5]),
-                marginBottom:parseInt(data[6]),
-                marginLeft:parseInt(data[7]),
-                marginRight:parseInt(data[8]),
-                monitorIndex:parseInt(data[9])
+                x: parseInt(data[0]),
+                y: parseInt(data[1]),
+                width: parseInt(data[2]),
+                height: parseInt(data[3]),
+                zoom: parseFloat(data[4]),
+                marginTop: parseInt(data[5]),
+                marginBottom: parseInt(data[6]),
+                marginLeft: parseInt(data[7]),
+                marginRight: parseInt(data[8]),
+                monitorIndex: parseInt(data[9]),
             });
-            let datavariant = new GLib.Variant ('a{sd}', {
-                x:parseInt(data[0]),
-                y:parseInt(data[1]),
-                width:parseInt(data[2]),
-                height:parseInt(data[3]),
-                zoom:parseFloat(data[4]),
-                marginTop:parseInt(data[5]),
-                marginBottom:parseInt(data[6]),
-                marginLeft:parseInt(data[7]),
-                marginRight:parseInt(data[8]),
-                monitorIndex:parseInt(data[9])
+            dataVariant = new GLib.Variant('a{sd}', {
+                x: parseInt(data[0]),
+                y: parseInt(data[1]),
+                width: parseInt(data[2]),
+                height: parseInt(data[3]),
+                zoom: parseFloat(data[4]),
+                marginTop: parseInt(data[5]),
+                marginBottom: parseInt(data[6]),
+                marginLeft: parseInt(data[7]),
+                marginRight: parseInt(data[8]),
+                monitorIndex: parseInt(data[9]),
             });
-            desktopVariants.push(datavariant);
+            desktopVariants.push(dataVariant);
             break;
         case '-V':
             version = arg;
@@ -137,15 +146,14 @@ function parseCommandLine(argv) {
         }
         lastCommand = null;
     }
-    if ((desktops.length == 0) && !asDesktop) {
+    if ((desktops.length === 0) && !asDesktop) {
         /* if no desktop list is provided, like when launching the program in stand-alone mode,
          * configure a 1280x720 desktop
          */
-        desktops.push({x:0, y:0, width: 1280, height: 720, zoom: 1, marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0, monitorIndex: 0});
+        desktops.push({ x: 0, y: 0, width: 1280, height: 720, zoom: 1, marginTop: 0, marginBottom: 0, marginLeft: 0, marginRight: 0, monitorIndex: 0 });
     }
-    for(let desktop of desktops) {
+    for (let desktop of desktops)
         desktop.primaryMonitor = primaryIndex;
-    }
 }
 
 parseCommandLine(ARGV);
@@ -165,8 +173,8 @@ PromiseUtils._promisify({}, Gio.FileEnumerator.prototype, 'close_async');
 PromiseUtils._promisify({}, Gio.FileEnumerator.prototype, 'next_files_async');
 PromiseUtils._promisify({}, Gio.Subprocess.prototype, 'wait_check_async');
 
-const fileProto = imports.system.version >= 17200 ?
-    Gio.File.prototype : Gio._LocalFilePrototype;
+const fileProto = imports.system.version >= 17200
+    ? Gio.File.prototype : Gio._LocalFilePrototype;
 
 PromiseUtils._promisify({}, fileProto, 'delete_async');
 PromiseUtils._promisify({}, fileProto, 'enumerate_children_async');
@@ -175,10 +183,10 @@ PromiseUtils._promisify({}, fileProto, 'make_directory_async');
 PromiseUtils._promisify({}, fileProto, 'query_info_async');
 PromiseUtils._promisify({}, fileProto, 'set_attributes_async');
 
-let localePath = GLib.build_filenamev([codePath, "locale"]);
-if (Gio.File.new_for_path(localePath).query_exists(null)) {
-    Gettext.bindtextdomain("ding", localePath);
-}
+let localePath = GLib.build_filenamev([codePath, 'locale']);
+if (Gio.File.new_for_path(localePath).query_exists(null))
+    Gettext.bindtextdomain('ding', localePath);
+
 
 const DesktopManager = imports.desktopManager;
 
@@ -200,8 +208,10 @@ if (asDesktop) {
 }
 
 // Use different AppIDs to allow to test it from a command line while the main desktop is also running from the extension
-const dingApp = new Gtk.Application({application_id: asDesktop ? 'com.rastersoft.ding' : 'com.rastersoft.dingtest',
-                                     flags: Gio.ApplicationFlags.HANDLES_COMMAND_LINE | Gio.ApplicationFlags.REPLACE});
+const dingApp = new Gtk.Application({
+    application_id: asDesktop ? 'com.rastersoft.ding' : 'com.rastersoft.dingtest',
+    flags: Gio.ApplicationFlags.HANDLES_COMMAND_LINE | Gio.ApplicationFlags.REPLACE,
+});
 
 dingApp.connect('startup', () => {
     Prefs.init(codePath, Enums);
@@ -211,39 +221,39 @@ dingApp.connect('startup', () => {
 dingApp.connect('activate', () => {
     if (!desktopManager) {
         desktopManager = new DesktopManager.DesktopManager(dingApp,
-                                                           dbusManager,
-                                                           desktops,
-                                                           codePath,
-                                                           asDesktop,
-                                                           primaryIndex,
-                                                           version);
+            dbusManager,
+            desktops,
+            codePath,
+            asDesktop,
+            primaryIndex,
+            version);
     }
 });
 
 dingApp.connect('command-line', (app, commandLine) => {
-    let argv =[];
+    let argv = [];
     argv = commandLine.get_arguments();
     parseCommandLine(argv);
-    if (! errorFound) {
-        if (commandLine.get_is_remote()) {
+    if (!errorFound) {
+        if (commandLine.get_is_remote())
             desktopManager.updateGridWindows(desktops);
             // If testing Dbus activations, comment the above and uncomment the following
-            //remoteDingActions.activate_action('updateGridWindows', new GLib.Variant('av', desktopVariants));
-        } else {
+            // remoteDingActions.activate_action('updateGridWindows', new GLib.Variant('av', desktopVariants));
+        else
             dingApp.activate();
-        }
+
         commandLine.set_exit_status(0);
     } else {
         commandLine.set_exit_status(1);
     }
 });
 
-if (!errorFound) {
+if (!errorFound)
     dingApp.run(ARGV);
-}
 
-if (!errorFound) {
+
+if (!errorFound)
     0;
-} else {
+else
     1;
-}
+
