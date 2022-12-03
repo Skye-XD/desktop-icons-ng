2a4d220a3dd7d8abe2512a8480d156bedc36eece
Sundeep Mediratta <smedius@gmail.com>
2022-12-03 13:28:29 -0500

    Fix local drop regression

--- app/desktopManager.js
+++ app/desktopManager.js
@@ -721,6 +721,8 @@ var DesktopManager = class {
 
         if (acceptFormat === this.Enums.DndTargetInfo.GNOME_ICON_LIST)
             fileList = GLib.Uri.list_extract_uris(dropData);
+        else if(acceptFormat === this.Enums.DndTargetInfo.DING_ICON_LIST)
+            fileList = dropData.get_files();
         else
             fileList = dropData.slice(1).split(' /').map(f => `file:///${f}`);
 
