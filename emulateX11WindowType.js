6325cc073ea57c26d11287b29f0f5aecde88084a
Sundeep Mediratta <smedius@gmail.com>
2023-05-03 09:23:15 -0400

    Simplify workspace switch

--- emulateX11WindowType.js
+++ emulateX11WindowType.js
@@ -349,17 +349,11 @@ var EmulateX11WindowType = class {
 
                     if (checkWorkspace) {
                         // activate the top-most window
-                        let windows = global.display.get_tab_list(Meta.TabList.NORMAL_ALL, global.workspace_manager.get_active_workspace());
-                        let anyActive = false;
-                        for (let window of windows) {
-                            if ((!window.customJS_ding || !window.customJS_ding._keepAtBottom) && !window.minimized) {
-                                Main.activateWindow(window);
-                                anyActive = true;
-                                break;
-                            }
-                        }
-                        if (!anyActive) {
-                            for (let window of this._windowList) {
+                        let window = global.display.get_tab_current(Meta.TabList.NORMAL, global.workspace_manager.get_active_workspace());
+                        if (window && (!window.customJS_ding || !window.customJS_ding._keepAtBottom) && !window.minimized) {
+                            Main.activateWindow(window);
+                        } else {
+                            for (window of this._windowList) {
                                 if (window.customJS_ding && window.customJS_ding._keepAtBottom && !window.minimized) {
                                     Main.activateWindow(window);
                                     break;
