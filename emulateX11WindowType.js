9822abba51d280220c5f04fb674ffba701ea8503
Sundeep Mediratta <smedius@gmail.com>
2023-10-14 20:41:43 -0400

    this._windowlist needs to be initialized properly and not null in the constructor.
    When enable() and disable() are called rapidly on the lock screen, disable() can be called before this,_windowList is initialized
    in emulateX11.enable(). The disable() call calls emulatedX11.disable(), that does not expect this._windowList to be null.
    If it is null the extension disable() call fails resulting in generalized mayhem and multiple DING processes
    being launched.
    
    Thanks to extensioons matrix chat channel for help in coming up with this fix.

--- emulateX11WindowType.js
+++ emulateX11WindowType.js
@@ -314,7 +314,7 @@ var EmulateX11WindowType = class {
      */
     constructor() {
         this._isX11 = !Meta.is_wayland_compositor();
-        this._windowList = null;
+        this._windowList = new Set();
         this._overviewHiding = true;
         this._waylandClient = null;
     }
@@ -328,9 +328,6 @@ var EmulateX11WindowType = class {
     }
 
     enable() {
-        if (!this._windowList)
-            this._windowList = new Set();
-
         this._idMap = global.window_manager.connect_after('map', (obj, windowActor) => {
             let window = windowActor.get_meta_window();
             if (this._waylandClient && this._waylandClient.query_window_belongs_to(window))
