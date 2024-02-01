435d7d42029e77cca0063a14562a412c8e180045
Sundeep Mediratta <smedius@gmail.com>
2024-01-31 20:48:05 -0500

    Only show option for choosing other file managers if they are available.

--- app/fileItemMenu.js
+++ app/fileItemMenu.js
@@ -306,7 +306,9 @@ const FileItemMenu = class {
                 openMenu.append(_('Open All...'), 'app.openMultipleFileAction');
             } else {
                 let app;
-                if (this.activeFileItem.attributeContentType === 'inode/directory')
+                if (this.activeFileItem.attributeContentType === 'inode/directory' &&
+                    Gio.AppInfo.get_all_for_type('inode/directory').length > 1
+                )
                     app = Gio.AppInfo.get_default_for_type(this.activeFileItem.attributeContentType, false)?.get_name();
                 else
                     app = Gio.AppInfo.get_default_for_type(this.activeFileItem.attributeContentType, true)?.get_name();
