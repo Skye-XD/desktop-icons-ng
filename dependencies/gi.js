4fc4181ef60ddbbfd41c2187e312be2989150666
Sundeep Mediratta <smedius@gmail.com>
2024-04-12 21:00:08 -0400

    Fix dependencies again

--- dependencies/gi.js
+++ dependencies/gi.js
@@ -6,13 +6,15 @@ import GdkX11 from 'gi://GdkX11?version=4.0';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 let GLibUnix;
-GLibUnix = await import('gi://GLibUnix').then(module => module.default).catch(e => {
+GLibUnix = await import('gi://GLibUnix').then(module => module.default).catch(_e => {
     console.log('GLibUnix not found.');
+});
+if (!GLibUnix) {
     console.log('Falling back to GLib...');
     GLibUnix = {
-        signal_add_full: GLib.unix_signal_add_full,
+        'signal_add_full': GLib.unix_signal_add,
     };
-});
+}
 import GnomeDesktop from 'gi://GnomeDesktop?version=4.0';
 const GnomeAutoar = await import('gi://GnomeAutoar').then(module => module.default).catch(e => console.error(e));
 import GObject from 'gi://GObject';
