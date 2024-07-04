a7840d6a73b4288b456e35cc21d08e6147f3065e
Sundeep Mediratta <smedius@gmail.com>
2024-07-04 09:22:36 -0400

    Fix Gio._promisify error in autoAr.js

--- app/autoAr.js
+++ app/autoAr.js
@@ -18,6 +18,11 @@
 import {Gtk, GLib, Gio, GnomeAutoar} from '../dependencies/gi.js';
 import {_} from '../dependencies/gettext.js';
 
+
+const fileProto = imports.system.version >= 17200
+    ? Gio.File.prototype : Gio._LocalFilePrototype;
+Gio._promisify(fileProto, 'make_directory_async');
+
 const Signals = imports.signals;
 
 export {AutoAr};
