75c9e3ba671f2866bf7d427997b1f7dcfb87b8cd
Sundeep Mediratta <smedius@gmail.com>
2024-12-26 08:15:41 -0500

    Sync Settings and the radio Menu
    Add a sync function
    
    Changing preferences arrangeorder will activate the action to the new change
    
    Changing the radiomenu will activate the action and change the settings to the new settings.
    
    If nothing is changed, the desktop will still be refreshed to the currently set arrangeorder
    
    Do not sort the icons twice.
    
    Setting the desktopSetting for arrange order on  radio button menu clicks calls the sync function recursively, so it is called twice with the same value.
    
    This sorts the icons twice on the desktop.
    
    We cannot block the signal handler for the desktopSettings to prevent this.
    
    Build in logic to the sync function so that sorting the icons happens exactly once even when called recursively.
    
    Block the action on the second call as we cannot block the signal

--- app/preferences.js
+++ app/preferences.js
@@ -181,7 +181,9 @@ const Preferences = class {
             }
             if (key === this._Enums.SortOrder.ORDER) {
                 this.sortOrder = this.desktopSettings.get_enum(this._Enums.SortOrder.ORDER);
-                this._desktopManager.onSortOrderChanged();
+                const value = GLib.Variant.new_string(
+                    this.desktopSettings.get_string(this._Enums.SortOrder.ORDER));
+                this._desktopManager.syncArrangeOrder(this._desktopManager.arrangeAction, value);
                 return;
             }
             if (key === 'unstackedtypes') {
