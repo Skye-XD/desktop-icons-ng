da8e88d0899e45e792d208b0cc19ecaf3e636b51
Sundeep Mediratta <smedius@gmail.com>
2025-04-10 11:53:46 -0400

    Make SymLinkItem Class
    Symlink class extends methods applying to symlinks  to the fileItem class.
    
    This again simplifies fileItem class
    
    Add emblems to symlinks
    
    Fix symlinkIcon

--- app/specialFolderIcon.js
+++ app/specialFolderIcon.js
@@ -25,6 +25,7 @@ import {_} from '../dependencies/gettext.js';
 
 export {SpecialFolderIcon};
 export {VolumeIcon};
+export {SymLinkIcon};
 
 const SpecialFolderIcon = class extends FileItem {
     constructor(desktopMnager, file, fileInfo, fileExtra, custom) {
@@ -281,5 +282,136 @@ const VolumeIcon = class extends FileItem {
             return this._custom.can_unmount();
         else
             return false;
+    }
+};
+
+const SymLinkIcon = class extends FileItem {
+    constructor(desktopManager, file, fileInfo, fileExtra, custom) {
+        super(desktopManager, file, fileInfo, fileExtra, custom);
+
+        this._isSymlink = fileInfo.get_attribute_boolean(
+            Gio.FILE_ATTRIBUTE_STANDARD_IS_SYMLINK
+        );
+
+        /*
+         * This is a glib trick to detect broken symlinks. If a file is a
+         * symlink, the filetype points to the final file, unless it is broken;
+         * thus if the file type is SYMBOLIC_LINK, it must be a broken link.
+         * https://developer.gnome.org/gio/stable/GFile.html#g-file-query-info
+         */
+        this._isBrokenSymlink =
+            this._isSymlink &&
+            this._fileType === Gio.FileType.SYMBOLIC_LINK;
+
+        if (this._isSymlink && !this._symlinkFileMonitor)
+            this._monitorSymlink();
+    }
+
+    async _updateMetadataFromFileInfo(fileInfo) {
+        this._isSymlink = fileInfo.get_attribute_boolean(
+            Gio.FILE_ATTRIBUTE_STANDARD_IS_SYMLINK
+        );
+
+        /*
+         * This is a glib trick to detect broken symlinks. If a file is a
+         * symlink, the filetype points to the final file, unless it is broken;
+         * thus if the file type is SYMBOLIC_LINK, it must be a broken link.
+         * https://developer.gnome.org/gio/stable/GFile.html#g-file-query-info
+         */
+        this._isBrokenSymlink =
+            this._isSymlink &&
+            this._fileType === Gio.FileType.SYMBOLIC_LINK;
+
+        await super._updateMetadataFromFileInfo(fileInfo);
+    }
+
+    _destroy() {
+        super._destroy();
+
+        if (this._symlinkFileMonitorId) {
+            this._symlinkFileMonitor.disconnect(this._symlinkFileMonitorId);
+            this._symlinkFileMonitor.cancel();
+            this._symlinkFileMonitorId = 0;
+        }
+    }
+
+    async _doOpenContext(context, fileList) {
+        if (!fileList)
+            fileList = [];
+
+        if (this._isBrokenSymlink) {
+            try {
+                console.log(
+                    `Error: Can’t open ${this.file.get_uri()}` +
+                    ' because it is a broken symlink.'
+                );
+
+                const title = _('Broken Link');
+                const error =
+                    _('Can not open this File because it is a Broken Symlink');
+
+                this._showerrorpopup(title, error);
+            } catch (e) {}
+
+            return;
+        }
+
+        await super._doOpenContext(context, fileList);
+    }
+
+    _monitorSymlink() {
+        let symlinkTarget = this._fileInfo.get_symlink_target();
+        let symlinkTargetGioFile = Gio.File.new_for_path(symlinkTarget);
+
+        this._symlinkFileMonitor = symlinkTargetGioFile.monitor(
+            Gio.FileMonitorFlags.WATCH_MOVES,
+            null
+        );
+
+        this._symlinkFileMonitor.set_rate_limit(1000);
+
+        this._symlinkFileMonitorId = this._symlinkFileMonitor.connect(
+            'changed',
+            this._updateSymlinkIcon.bind(this)
+        );
+    }
+
+    async _updateSymlinkIcon() {
+        await this._reloadIcon().catch(e => {
+            if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED)) {
+                console.error(
+                    e,
+                    `Exception while updating ${
+                        this._getVisibleName()
+                            ? this._getVisibleName()
+                            : 'symlink icon'
+                    }: ${e.message}`);
+            }
+        });
+    }
+
+    _addEmblemsToIconIfNeeded(iconPaintable, position = 0) {
+        let emblem = null;
+        let newIconPaintable = iconPaintable;
+
+        if (this._isSymlink && this.Prefs.showLinkEmblem) {
+            emblem = Gio.ThemedIcon.new('icon-emblem-symbolic-link');
+
+            newIconPaintable =
+                this._addEmblem(newIconPaintable, emblem, position);
+
+            position += 1;
+        }
+
+        if (this._isBrokenSymlink) {
+            emblem = Gio.ThemedIcon.new('icon-emblem-unreadable');
+
+            newIconPaintable =
+                this._addEmblem(newIconPaintable, emblem, position);
+
+            position += 1;
+        }
+
+        return super._addEmblemsToIconIfNeeded(newIconPaintable, position);
     }
 };
