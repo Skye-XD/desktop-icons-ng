8f65ec0a9f52944cc95f5bce1fe50a1d2c836de3
Sundeep Mediratta <smedius@gmail.com>
2025-04-14 06:20:23 -0400

    Remove unused code, clean up

--- app/desktopMenu.js
+++ app/desktopMenu.js
@@ -15,10 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-    TemplatesScriptsManager
-} from '../dependencies/localFiles.js';
-
 import {Gdk, Gio, GLib, Gtk} from '../dependencies/gi.js';
 import {_} from '../dependencies/gettext.js';
 
