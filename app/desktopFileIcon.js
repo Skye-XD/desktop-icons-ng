ce74d559137e91d3633d149c809edfad23fc7f44
Sundeep Mediratta <smedius@gmail.com>
2025-05-08 20:27:39 -0400

    Show the correct error if the Desktop Folder is writable by others.

--- app/desktopFileIcon.js
+++ app/desktopFileIcon.js
@@ -80,6 +80,19 @@ const DesktopFileIcon = class extends FileItemIcon {
     }
 
     _launchDesktopFile(context, fileList) {
+        if (this._desktopManager.writableByOthers) {
+            const title = _('The Displayed Desktop is writable by others');
+            const error =
+                _(
+                    '.deskop files cannot be launched from this Desktop' +
+                    ' as the Desktop Folder is writable by other users.\n\n' +
+                    'Please check the permissions of this Desktop Folder,' +
+                    ' and make sure it is not writable by others.'
+                );
+
+            this._showerrorpopup(title, error);
+            return;
+        }
         if (!this._isValidDesktopFile) {
             const title = _('Broken Desktop File');
             const error =
