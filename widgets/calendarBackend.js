3f87b4aa7b7fc7e02be3277cdf48b91f12dc1972
Sundeep Mediratta <smedius@gmail.com>
2026-01-09 10:47:08 -0500

    Remove duplicate header

--- widgets/calendarBackend.js
+++ widgets/calendarBackend.js
@@ -13,23 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/* DING: Desktop Icons New Generation for GNOME Shell
- *
- * Gtk4 Port Copyright (C) 2022 - 2026 Sundeep Mediratta (smedius@gmail.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Calendar Backend
